1438a433ef10ec129822acac53da66b4fc864c07
Alex Popov <yolloartstudio@gmail.com>
2020-08-24 00:04:03 +0300

    Final version

--- RGBslider/ViewController.swift
+++ RGBslider/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IB Outlets
+    @IBOutlet weak var rgbView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    //MARK: - Life Cycles Metod
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.text = "0.00"
+        greenLabel.text = "0.00"
+        blueLabel.text = "0.00"
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        let sliderValueRed = CGFloat(redSlider.value)
+        let sliderValueGreen = CGFloat(greenSlider.value)
+        let sliderValueBlue = CGFloat(blueSlider.value)
+        
+        let currentColor = UIColor(red: sliderValueRed, green: sliderValueGreen, blue: sliderValueBlue, alpha: 1)
+        rgbView.backgroundColor = currentColor
+    }
+    
+    //MARK: - IB Actions
+    @IBAction func redSliderAction()  {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    
 }
+
 
