aa415501201044d8c6c7c6d8387209f43a4bde9d
Alex Popov <yolloartstudio@gmail.com>
2020-08-23 16:11:29 +0300

    Initial Commit

---
+++ RGBslider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBslider
+//
+//  Created by YolloArts on 23.08.2020.
+//  Copyright © 2020 YolloArts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
